9aad0eee8314bc16f03b215888cf16d7610431d0
pedrohnsilva4 <pedro.hnsilva4@senacsp.edu.br>
2026-06-15 22:06:44 -0300

    Add files via upload

---
+++ _header.php
@@ -0,0 +1,19 @@
+<?php
+$base_url = 'http://localhost/projeto99/';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="<?= $base_url ?>admin/home.php">Home</a></li>
+            <li><a href="<?= $base_url ?>admin/site/index.php>">Products</li>
+        </ul>
+    </nav>
+</body>
+</html>
